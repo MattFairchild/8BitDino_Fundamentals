cc9cb06adec4839df4e5fc10bb168b0ab7a702f9
MattFairchild <matt_fv1@hotmail.com>
2021-11-18 01:45:57 +0100

    remove function in dinomenu that is too specific

--- Runtime/_Scripts/Menu/DinoMenu.cs
+++ Runtime/_Scripts/Menu/DinoMenu.cs
@@ -22,27 +22,9 @@ namespace EightBitDinosaur
 	    #endregion
 	
 	    #region MENU FUNCTIONS
-	
-	    public static GameObject spawn_endpanel()
-	    {
-	        if (ActiveMenu != null)
-	        {
-	            ActiveMenu.close();
-	            ActiveMenu = null;
-	        }
-	
-	        GameObject result = null;
-	        GameObject prefab = Resources.Load<GameObject>("Menu1Button");
-	        result = GameObject.Instantiate(prefab);
-	
-	        ActiveMenu = result.GetComponent<Menu>() ?? result.AddComponent<Menu>();
-	        LoadLevel loadlevel_script = result.AddComponent<LoadLevel>();
-	
-	        ActiveMenu.assign_button(1, () => { loadlevel_script.load_level(0); });
-	        
-	        return result;
-	    }
-	
+
+
+
 	    #endregion
 	}
 }
